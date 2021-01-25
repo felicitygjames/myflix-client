c813e1b88320d94dbb838d463c7fa0186b7970a5
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2021-01-25 17:55:21 -0600

    changes with Sammy to finish 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,7 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 
 export class MainView extends React.Component {
@@ -23,16 +24,28 @@ export class MainView extends React.Component {
     }
   
     componentDidMount() {
-        axios.get('https://myflixmovies-fj.herokuapp.com/movies')
-          .then(response => {
-            // Assign the result to the state
-            this.setState({
-              movies: response.data
-            });
-          })
-          .catch(function (error) {
-            console.log(error);
+      let accessToken = localStorage.getItem('token');
+      if (accessToken !== null) {
+        this.setState({
+          user: localStorage.getItem('user')
+        });
+        this.getMovies(accessToken);
+      }
+    }
+
+      getMovies(token) {
+        axios.get('https://myflixmovies-fj.herokuapp.com/movies', {
+          headers: { Authorization: `Bearer ${token}`}
+        })
+        .then(response => {
+          // Assign the result to the state
+          this.setState({
+            movies: response.data
           });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
       }
 
       onMovieClick(movie) {
@@ -41,11 +54,17 @@ export class MainView extends React.Component {
         });
       }
 
-      onLoggedIn(user) {
+      onLoggedIn(authData) {
+        console.log(authData);
         this.setState({
-          user
+          user: authData.user.Username
         });
+      
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getMovies(authData.token);
       }
+      
 
     // This overrides the render() method of the superclass
     // No need to call super() though, as it does nothing by default
@@ -56,6 +75,28 @@ export class MainView extends React.Component {
         if (!movies) return <div className="main-view"/>;
 
         return (
+          <div>
+            <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
           <Row className="main-view justify-content-md-center">
             {selectedMovie
               ? (
@@ -70,6 +111,7 @@ export class MainView extends React.Component {
                 ))
             }
           </Row>
+          </div>
         );
     }
   }
