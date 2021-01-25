4d663ed1f6567da60dd91b150e8179bc214acbdf
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2021-01-25 15:59:49 -0600

    integrated views into main view and boostrap into index.html

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import {MovieCard} from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+
 
 export class MainView extends React.Component {
     constructor() {
