e4494ef447b57c5f374087b938e3a4380d84f5ea
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2021-02-13 16:08:20 -0600

    added redux etc

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import {MainView} from './components/main-view/main-view'
-// Import statement to indicate that you need to bundle `./index.scss`
+import moviesApp from './reducers/reducers.js';
 import './index.scss';
+const store = createStore(moviesApp);
 import Container from 'react-bootstrap/Container';
 
-// Main component (will eventually use all the others)
+
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-      <MainView />
-      </Container>
+      <Provider store={store}>
+        <MainView />
+    </Provider>
     );
   }
 }
 
-// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
